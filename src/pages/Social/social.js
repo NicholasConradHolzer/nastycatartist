e084152700d10cc7ebe43e8ceca0c8877953835a
NicholasCH <nicholasch96@gmail.com>
2023-04-24 15:53:23 -0500

    attempt 4

--- src/pages/Social/social.js
+++ src/pages/Social/social.js
@@ -8,34 +8,7 @@ import instagram from "../../components/Embeds/instagram.js";
 
 
 export default function Social(){
-    const projectArray = [
-        {
-            title: 'Fanpage: Nyango-Starr',
-            image: nyango,
-            alt:'homepage for Nyango-Starr Fanclub',
-            linkDeployed: 'https://nicholasconradholzer.github.io/NHolzerNyangoStarr/',
-            // linkGithub:'',
-            description:'A Fanpage for Japanese Mascot/Drummer/Apple-Cat Nyango Starr; featuring embedded youtube video as a background color for the site.',
-            tech:''
-        },
-        {
-            title: 'My "Watch-the-Box" Page',
-            image: watchBox,
-            alt:'homepage for Watch-the-Box',
-            linkDeployed: 'https://nicholasconradholzer.github.io/boxManipulator/',
-            // linkGithub:'',
-            description:'A "Watch-the-Box" page; with buttons that use javascript to directly alter the CSS styling of a colored-box.',
-            tech:''
-        },
-        {
-            title: 'Instagram',
-            image: runBuddy,
-            alt:'homepage for "Run-Buddy, Inc."',
-            linkDeployed: 'https://nicholasconradholzer.github.io/run-buddy/',
-            // linkGithub:'',
-            description:'The homepage and privacy policy page of Run-Buddy, Inc., featuring advanced applications of CSS for both user responsiveness and screensize-disparity formatting.',
-            tech:''
-        }]
+    
     return(
         <div id="work-samples-nick" className="work-display">
         <div id="samplesHolder">
@@ -43,8 +16,7 @@ export default function Social(){
                Social Media Links!
             </h2>
         </div>
-            <div>
-           {instagram}</div>
+            <div>{instagram()}</div>
         </div>
     )
 }
